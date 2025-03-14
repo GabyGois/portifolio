7bc6f8fe03d76b1e57d47f4c96aba9bb3671aea5
GabyGois <gabriela.mendonca.gois@escola.pr.gov.br>
2025-03-14 12:04:24 -0300

    mudanças

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu() {
+    var menu = document.getElementById("links");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
