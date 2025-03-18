abc6b765b03271983f3c2136a76aa2687afc5454
GabyGois <141441773+GabyGois@users.noreply.github.com>
2025-03-17 21:40:32 -0300

    ajuste identação

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // addEventListener -> ao carregar(DOMContentLoaded) a pagina(document) executa a função
-    const path = window.location.pathname; // captura a url da pagina
-    let novaCor = '';
-  
-    // Verifica se a URL contem o texto passado, verificando a página no processo
-    if (path.includes('fisica.html')) {
-      novaCor = '#FFA725';
-    } else if (path.includes('matematica.html')) {
-      novaCor = '#161179';
-    } else if (path.includes('quimica.html')) {
-      novaCor = '#211C84';
-    } else if (path.includes('biologia.html')) {
-      novaCor = '#27391C';
-    } else {
-      novaCor = '#333'; // cor padrão para a página index
-    }
-  
-    // Atualiza a variável CSS no :root
-    document.documentElement.style.setProperty('--cor-header', novaCor);
-  });
+  // addEventListener -> ao carregar(DOMContentLoaded) a pagina(document) executa a função
+  const path = window.location.pathname; // captura a url da pagina
+  let novaCor = '';
+
+  // Verifica se a URL contem o texto passado, verificando a página no processo
+  if (path.includes('fisica.html')) {
+    novaCor = '#FFA725';
+  } else if (path.includes('matematica.html')) {
+    novaCor = '#161179';
+  } else if (path.includes('quimica.html')) {
+    novaCor = '#211C84';
+  } else if (path.includes('biologia.html')) {
+    novaCor = '#27391C';
+  } else {
+    novaCor = '#333'; // cor padrão para a página index
+  }
+
+  // Atualiza a variável CSS no :root
+  document.documentElement.style.setProperty('--cor-header', novaCor);
+});
   
