35789758f4a1246b5f739166685d47b10a7fb18e
GabyGois <141441773+GabyGois@users.noreply.github.com>
2025-04-15 11:34:14 -0300

    asd

--- script.js
+++ script.js
@@ -25,33 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Atualiza a variável CSS no :root
   document.documentElement.style.setProperty('--cor-header', novaCor);
-  
-  function adicionarTarefa(materia) {
-    Swal.fire({
-      title: 'Login Form',
-      html: `
-        <input type="text" id="username" class="swal2-input" placeholder="Username">
-        <input type="password" id="password" class="swal2-input" placeholder="Password">
-      `,
-      confirmButtonText: 'Sign in',
-      focusConfirm: false,
-      didOpen: () => {
-        const popup = Swal.getPopup()
-        usernameInput = popup.querySelector('#username')
-        passwordInput = popup.querySelector('#password') 
-        usernameInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
-        passwordInput.onkeyup = (event) => event.key === 'Enter' && Swal.clickConfirm()
-      },
-      preConfirm: () => {
-        const username = usernameInput.value
-        const password = passwordInput.value
-        if (!username || !password) {
-          Swal.showValidationMessage(`Please enter username and password`)
-        }
-        return { username, password }
-      },
-    })
-  }
 });
 
   
